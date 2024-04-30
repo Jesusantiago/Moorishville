32f1c9088c0b47d0cc44f136492d6ea46695749c
Jesus Santiago <jesusantiago420@gmail.com>
2024-04-30 17:18:35 -0300

    ADD first view and Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="  w-screen h-32 bg-mostaza flex">
+      <div className="logo w-2/12 bg-bordo"></div>
+      <div className="text w-full bg-white">
+        <h2 className="text-3xl font-bold font-display text-bordo">
+          MOORISHVILLE COUNTY DIVINE - REGISTER OF DEEDS
+        </h2>
+        <h4 className="text-2xl font-bold font-display">Moorishville County divine - Register of Deeds </h4>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="logo w-2/12 bg-red"></div>
+    </div>
+
     </>
   )
 }
